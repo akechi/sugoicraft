e902ca3349feab539c6f1abdf43eee28bafa3e5f
ujihisa <ujihisa@gmail.com>
2014-03-14 21:27:04 -0700

    togglesplint sample

--- src/main/scala/io/github/akechi/sugoicraft/SugoiPlugin.scala
+++ src/main/scala/io/github/akechi/sugoicraft/SugoiPlugin.scala
@@ -1,9 +1,16 @@
 package io.github.akechi.sugoicraft
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.event.Listener
+import org.bukkit.event.{Listener,EventHandler}
 
 class SugoiPlugin extends JavaPlugin with Listener {
   override def onEnable() {
     println('cool)
+    getServer().getPluginManager().registerEvents(this, this)
+  }
+
+  @EventHandler
+  def onPlayerToggleSprint(evt: org.bukkit.event.player.PlayerToggleSprintEvent) {
+    val player = evt.getPlayer
+    println(player, evt.isSprinting)
   }
 }
