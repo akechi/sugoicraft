4cd350fb98e3346c9bb21a0a4cb212204e519c9c
ujihisa <ujihisa@gmail.com>
2014-03-14 20:22:15 -0700

    override isn't annotation

--- src/main/scala/io/akechi/sugoicraft/SugoiPlugin.scala
+++ src/main/scala/io/akechi/sugoicraft/SugoiPlugin.scala
@@ -1,4 +1,9 @@
 package io.akechi.sugoicraft
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.event.Listener
 
-class SugoiPlugin {
+class SugoiPlugin extends JavaPlugin with Listener {
+  override def onEnable() {
+    println('cool)
+  }
 }
