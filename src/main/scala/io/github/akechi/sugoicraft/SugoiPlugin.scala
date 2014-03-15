42077425c24984d84ca539e1fb63dd6cf0ac92cb
ujihisa <ujihisa@gmail.com>
2014-03-15 09:38:34 -0700

    no fast-dash on sand/gravel

--- src/main/scala/io/github/akechi/sugoicraft/SugoiPlugin.scala
+++ src/main/scala/io/github/akechi/sugoicraft/SugoiPlugin.scala
@@ -2,6 +2,7 @@ package io.github.akechi.sugoicraft
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.event.{Listener,EventHandler}
+import org.bukkit.Material
 
 class SugoiPlugin extends JavaPlugin with Listener {
   override def onEnable() {
@@ -9,10 +10,13 @@ class SugoiPlugin extends JavaPlugin with Listener {
     Bukkit.getPluginManager().registerEvents(this, this)
   }
 
+  val slowBlocks = Set(Material.SAND, Material.GRAVEL)
+
   @EventHandler
   def onPlayerToggleSprint(evt: org.bukkit.event.player.PlayerToggleSprintEvent) {
     val player = evt.getPlayer
-    if (evt.isSprinting) {
+    val loc = player.getLocation.clone.add(0, -1, 0)
+    if (evt.isSprinting && slowBlocks.contains(loc.getBlock.getType)) {
       player.setWalkSpeed(0.4F)
     } else {
       player.setWalkSpeed(0.2F)
