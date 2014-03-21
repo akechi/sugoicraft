1e2aa339c334c863062933ddf4ecbe7d5e7aa612
raa0121 <raa0121@gmail.com>
2014-03-22 06:30:51 +0900

    add DummyTest

---
+++ src/test/scala/io/github/akechi/sugoicraft/DummyTest.scala
@@ -0,0 +1,9 @@
+package io.github.akechi.sugoicraft
+
+import org.scalatest.FunSuite
+
+class HelloSuite extends FunSuite {
+  test("""traditional "hello, world" from K&R""") {
+    assert(Hello.greeting === "hello, world")
+  }
+}
