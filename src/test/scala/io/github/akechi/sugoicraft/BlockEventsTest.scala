2dc66aff3e4d404540677aaf72f9bdd34bb2cb82
meiraka <meiraka.pre@gmail.com>
2014-03-23 23:37:02 +0900

    add all test for current blockevents.

--- src/test/scala/io/github/akechi/sugoicraft/BlockEventsTest.scala
+++ src/test/scala/io/github/akechi/sugoicraft/BlockEventsTest.scala
@@ -10,23 +10,82 @@ import org.bukkit.{Material, GameMode, World, Location}
 import org.bukkit.block.Block
 import org.bukkit.inventory.ItemStack
 
+class BlockEventsForTest extends BlockEvents {
+  /** definition for special brock break events. */
+  override val damageBlocks = Map(
+    Material.GRASS -> PendingItem(
+      Material.GLASS),
+    Material.LEAVES -> PendingItem(
+      Material.MELON,
+      by = Set(Material.DIRT)),
+    Material.SMOOTH_BRICK -> PendingItem(
+      Material.SMOOTH_BRICK,
+      by = Set(Material.STONE_PICKAXE),
+      dataFrom = 1,
+      dataTo = 2))
+}
+
 class BlockEventsTest extends FunSpec {
   describe("BlockEventsTest") {
     describe("onBlockBreakEvent") {
       describe("event did not cancelled") {
+        it ("if player is not in survival.") {
+          val block = mock(classOf[Block])
+          val player = mock(classOf[Player])
+          val evt = new org.bukkit.event.block.BlockBreakEvent(block, player)
+          when(block.getType).thenReturn(Material.GRASS)
+          when(player.getGameMode).thenReturn(GameMode.CREATIVE)
+          val blockEvents = new BlockEventsForTest()
+          blockEvents.onBlockBreakEvent(evt)
+          assert(!evt.isCancelled)
+        }
         it ("if target block type does not matched.") {
           val block = mock(classOf[Block])
           val player = mock(classOf[Player])
           val evt = new org.bukkit.event.block.BlockBreakEvent(block, player)
           when(block.getType).thenReturn(Material.SAND)
           when(player.getGameMode).thenReturn(GameMode.SURVIVAL)
-          val blockEvents = new BlockEvents()
+          val blockEvents = new BlockEventsForTest()
+          blockEvents.onBlockBreakEvent(evt)
+          assert(!evt.isCancelled)
+        }
+        it ("if hand item type does not matched.") {
+          val targetMaterial = Material.LEAVES
+          val handMaterial = Material.STONE_PICKAXE
+          val block = mock(classOf[Block])
+          val player = mock(classOf[Player])
+          val evt = new org.bukkit.event.block.BlockBreakEvent(block, player)
+          val playerHandStack = mock(classOf[ItemStack])
+          when(block.getType).thenReturn(targetMaterial)
+          when(player.getGameMode).thenReturn(GameMode.SURVIVAL)
+          when(player.getItemInHand).thenReturn(playerHandStack)
+          when(playerHandStack.getType).thenReturn(handMaterial)
+          val blockEvents = new BlockEventsForTest()
+          blockEvents.onBlockBreakEvent(evt)
+          assert(!evt.isCancelled)
+        }
+        it ("if target block data does not matched.") {
+          val targetMaterial = Material.SMOOTH_BRICK
+          val targetData = 0.toByte
+          val handMaterial = Material.STONE_PICKAXE
+          val block = mock(classOf[Block])
+          val player = mock(classOf[Player])
+          val evt = new org.bukkit.event.block.BlockBreakEvent(block, player)
+          val playerHandStack = mock(classOf[ItemStack])
+          when(block.getType).thenReturn(targetMaterial)
+          when(block.getData).thenReturn(targetData)
+          when(player.getGameMode).thenReturn(GameMode.SURVIVAL)
+          when(player.getItemInHand).thenReturn(playerHandStack)
+          when(playerHandStack.getType).thenReturn(handMaterial)
+          val blockEvents = new BlockEventsForTest()
           blockEvents.onBlockBreakEvent(evt)
           assert(!evt.isCancelled)
         }
       }
       describe("dropped items and event cancelled") {
-        it ("if target block type, data, and hand material matched") {
+        it ("if target block type matched") {
+          val targetMaterial = Material.GRASS
+          val handMaterial = Material.STONE_PICKAXE
           val block = mock(classOf[Block])
           val blockLocation = mock(classOf[Location])
           val player = mock(classOf[Player])
@@ -34,12 +93,38 @@ class BlockEventsTest extends FunSpec {
           val playerHandStack = mock(classOf[ItemStack])
           val world = mock(classOf[World])
           when(player.getGameMode).thenReturn(GameMode.SURVIVAL)
-          when(block.getType).thenReturn(Material.SMOOTH_BRICK)
+          when(block.getType).thenReturn(targetMaterial)
           when(player.getItemInHand).thenReturn(playerHandStack)
-          when(playerHandStack.getType).thenReturn(Material.STONE_PICKAXE)
+          when(playerHandStack.getType).thenReturn(handMaterial)
           when(block.getWorld).thenReturn(world)
           when(block.getLocation).thenReturn(blockLocation)
-          val blockEvents = new BlockEvents()
+          val blockEvents = new BlockEventsForTest()
+          blockEvents.onBlockBreakEvent(evt)
+          val itemStackCaptor = ArgumentCaptor.forClass(classOf[ItemStack])
+          verify(world, times(1)).dropItemNaturally(
+            Matchers.eq(blockLocation),
+            itemStackCaptor.capture)
+          assert(Material.GLASS === itemStackCaptor.getValue.getType)
+          assert(evt.isCancelled)
+        }
+        it ("if target block type, data, and hand material matched") {
+          val targetMaterial = Material.SMOOTH_BRICK
+          val targetData = 1.toByte
+          val handMaterial = Material.STONE_PICKAXE
+          val block = mock(classOf[Block])
+          val blockLocation = mock(classOf[Location])
+          val player = mock(classOf[Player])
+          val evt = new org.bukkit.event.block.BlockBreakEvent(block, player)
+          val playerHandStack = mock(classOf[ItemStack])
+          val world = mock(classOf[World])
+          when(player.getGameMode).thenReturn(GameMode.SURVIVAL)
+          when(block.getType).thenReturn(targetMaterial)
+          when(block.getData).thenReturn(targetData)
+          when(player.getItemInHand).thenReturn(playerHandStack)
+          when(playerHandStack.getType).thenReturn(handMaterial)
+          when(block.getWorld).thenReturn(world)
+          when(block.getLocation).thenReturn(blockLocation)
+          val blockEvents = new BlockEventsForTest()
           blockEvents.onBlockBreakEvent(evt)
           val itemStackCaptor = ArgumentCaptor.forClass(classOf[ItemStack])
           verify(world, times(1)).dropItemNaturally(
