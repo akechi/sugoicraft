a6e312bb1725bad743a848f20443325bd20a8d67
ujihisa <ujihisa@gmail.com>
2014-03-24 10:34:01 -0700

    cool beans!

--- src/test/scala/io/github/akechi/sugoicraft/DummyTest.scala
+++ src/test/scala/io/github/akechi/sugoicraft/DummyTest.scala
@@ -2,8 +2,9 @@ package io.github.akechi.sugoicraft
 
 import org.scalatest.FunSuite
 
-class HelloSuite extends FunSuite {
+// class HelloSuite extends FunSuite {
+class HootSuite extends FunSuite {
   test("""traditional "hello, world" from K&R""") {
-    assert(false)
+    assert(true)
   }
 }
