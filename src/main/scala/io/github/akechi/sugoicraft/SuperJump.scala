2aea861f16e8a9fa5bea955a0fbd628a1edd3fe6
meiraka <meiraka.pre@gmail.com>
2014-04-07 22:38:13 +0900

    scala naming convention

--- src/main/scala/io/github/akechi/sugoicraft/SuperJump.scala
+++ src/main/scala/io/github/akechi/sugoicraft/SuperJump.scala
@@ -10,16 +10,16 @@ import org.bukkit.event.{Listener,EventHandler}
  */
 class SuperJump extends Listener {
   /** name, (count, timestamp) */
-   // meiraka TODO: use scala naming convention
-  var crouching_counter: Map[String, (Int, Long)] = Map()
+  var crouchingCounter: Map[String, (Int, Long)] = Map()
   
   def getCrouchingCounter(player: Player) : Int = {
     // meiraka TODO: use get() and pattern matching, or getOrElse()
-    if (!this.crouching_counter.contains(player.getName)) {
+    
+    if (!this.crouchingCounter.contains(player.getName)) {
       return 0
     } else {
       val current_time = System.currentTimeMillis / 1000
-      val (count, old_time) = this.crouching_counter(player.getName)
+      val (count, old_time) = this.crouchingCounter(player.getName)
       val new_count = count - (current_time - old_time).toInt
       if (new_count < 0) {
         return 0
@@ -31,7 +31,7 @@ class SuperJump extends Listener {
 
   def incrementCrouchingCounter(player: Player) {
     val current_time = System.currentTimeMillis / 1000
-    crouching_counter = crouching_counter.updated(
+    crouchingCounter = crouchingCounter.updated(
       player.getName,
       (this.getCrouchingCounter(player) + 1, current_time))
 
